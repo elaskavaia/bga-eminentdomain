5fb8d56a169ffd6ee365b5f0960dc516631c078e
Alex A. <github@xmakina.co.uk>
2024-04-23 08:02:05 +0100

    add scenario selection

--- gameoptions.inc.php
+++ gameoptions.inc.php
@@ -29,76 +29,126 @@ $game_options = array(
         100 => array(
                 'name' => totranslate('Learning Game (No Research)'),
                 'values' => array(
-                        
-                        1 => array( 'name' => totranslate('Off')), 
-                        2 => array( 'name' => totranslate('On'), 'tmdisplay' => totranslate('Learning Game') ),
-                        
+
+                        1 => array('name' => totranslate('Off')),
+                        2 => array('name' => totranslate('On'), 'tmdisplay' => totranslate('Learning Game')),
+
                 ),
                 'displaycondition' => array(
                         // Note: do not display this option unless these conditions are met
-                        array( 'type' => 'otheroption',
+                        array(
+                                'type' => 'otheroption',
                                 'id' => 201, // ELO OFF hardcoded framework option
                                 'value' => 1, // 1 if OFF
 
                         )
                 ),
                 'notdisplayedmessage' => totranslate('Learning variant available only with ELO off')
-                ),
-    
+        ),
+
         101 => array(
                 'name' => totranslate('Extended 3-Player Game'),
                 'values' => array(
-                        1 => array( 'name' => totranslate('Off'), 
-                                'nobeginner' => false  ),
-                        2 => array( 'name' => totranslate('On'), 'tmdisplay' => totranslate('Extended'),
-                                'nobeginner' => true  ),
-                        
+                        1 => array(
+                                'name' => totranslate('Off'),
+                                'nobeginner' => false
+                        ),
+                        2 => array(
+                                'name' => totranslate('On'), 'tmdisplay' => totranslate('Extended'),
+                                'nobeginner' => true
+                        ),
+
                 ),
         ),
         102 => array(
                 'name' => totranslate('Scenarios'),
                 'values' => array(
-                        1 => array( 'name' => totranslate('Off'),
-                                'nobeginner' => false  ),
-                        2 => array( 'name' => totranslate('On'), 'tmdisplay' => totranslate('Scenarios'),
-                                'nobeginner' => true  ),
-                        
+                        1 => array(
+                                'name' => totranslate('Off'),
+                                'nobeginner' => false
+                        ),
+                        2 => array(
+                                'name' => totranslate('On'), 'tmdisplay' => totranslate('Scenarios'),
+                                'nobeginner' => true
+                        ),
+
                 ),
                 'displaycondition' => array(
                         // Note: do not display this option unless these conditions are met
-                        array( 'type' => 'otheroptionisnot',
+                        array(
+                                'type' => 'otheroptionisnot',
                                 'id' => 100, // learning variant
                                 'value' => 2, // 1 if OFF,2 is ON
-                                
+
                         )
                 ),
                 'notdisplayedmessage' => totranslate('Scenarios variant is not available if Learning variant is chosen')
         ),
+        105 => array(
+                'alpha' => true,
+                'name' => totranslate('Scenario Selection'),
+                'values' => array(
+                        1 => array(
+                                'name' => totranslate('Off'),
+                                'nobeginner' => false
+                        ),
+                        2 => array(
+                                'name' => totranslate('On'), 'tmdisplay' => totranslate('Scenario Selection'),
+                                'nobeginner' => true
+                        ),
+
+                ),
+                'displaycondition' => array(
+                        array(
+                                'type' => 'otheroptionisnot',
+                                'id' => 100, // learning variant
+                                'value' => 2, // 1 if OFF,2 is ON
+
+                        ),
+                        array(
+                                'type' => 'otheroption',
+                                'id' => 102, // scenario variant
+                                'value' => 2, // 1 if OFF,2 is ON
+
+                        )
+                ),
+                'default' => 1,
+                'notdisplayedmessage' => totranslate('This variant is not available without Scenarios')
+        ),
         103 => array(
                 'name' => totranslate('Extra Turn after Game End Triggered'),
                 'values' => array(
-                        1 => array( 'name' => totranslate('Off'),
-                                'nobeginner' => false  ),
-                        2 => array( 'name' => totranslate('On'), 'tmdisplay' => totranslate('Extra Turn'),
-                                'nobeginner' => false  ),
-                        
+                        1 => array(
+                                'name' => totranslate('Off'),
+                                'nobeginner' => false
+                        ),
+                        2 => array(
+                                'name' => totranslate('On'), 'tmdisplay' => totranslate('Extra Turn'),
+                                'nobeginner' => false
+                        ),
+
                 ),
                 'default' => 2
         ),
         104 => array(
                 'name' => totranslate('Escalation Expansion'),
                 'values' => array(
-                        1 => array( 'name' => totranslate('Off'),
-                                'nobeginner' => false  ),
-                        2 => array( 'name' => totranslate('On'), 'tmdisplay' => totranslate('Escalation'),
-                                'nobeginner' => true  ),
-                        
+                        1 => array(
+                                'name' => totranslate('Off'),
+                                'nobeginner' => false
+                        ),
+                        2 => array(
+                                'name' => totranslate('On'), 'tmdisplay' => totranslate('Escalation'),
+                                'nobeginner' => true
+                        ),
+
                 ),
                 'displaycondition' => array(
-                        array( 'type' => 'otheroptionisnot',
+                        array(
+                                'type' => 'otheroptionisnot',
                                 'id' => 100, // learning variant
                                 'value' => 2, // 1 if OFF,2 is ON
-                                
+
                         )
                 ),
                 'default' => 1,
@@ -108,10 +158,10 @@ $game_options = array(
 );
 
 if (!defined('PREF_AUTO_DISSENT')) { // guard since this included multiple times
-    define("PREF_AUTO_DISSENT", 150);
-    define("PREFVALUE_AUTO_DISSENT_ON", 1);
-    define("PREFVALUE_AUTO_DISSENT_OFF", 0);
-    define("PREFVALUE_AUTO_DISSENT_TIMER", 2);
+        define("PREF_AUTO_DISSENT", 150);
+        define("PREFVALUE_AUTO_DISSENT_ON", 1);
+        define("PREFVALUE_AUTO_DISSENT_OFF", 0);
+        define("PREFVALUE_AUTO_DISSENT_TIMER", 2);
 }
 
 
@@ -120,9 +170,9 @@ $game_preferences = array(
                 'name' => totranslate('Auto Dissent'),
                 //'needReload' => true, // after user changes this preference game interface would auto-reload
                 'values' => array(
-                        PREFVALUE_AUTO_DISSENT_OFF => array( 'name' => totranslate( 'No Auto Dissent' ), 'cssPref' => 'dissent_manual' ),
-                        PREFVALUE_AUTO_DISSENT_ON => array( 'name' => totranslate( 'Auto Dissent' ), 'cssPref' => 'dissent_auto' ),
-                        PREFVALUE_AUTO_DISSENT_TIMER => array( 'name' => totranslate( 'Auto Dissent with Timer' ), 'cssPref' => 'dissent_timer' ),
+                        PREFVALUE_AUTO_DISSENT_OFF => array('name' => totranslate('No Auto Dissent'), 'cssPref' => 'dissent_manual'),
+                        PREFVALUE_AUTO_DISSENT_ON => array('name' => totranslate('Auto Dissent'), 'cssPref' => 'dissent_auto'),
+                        PREFVALUE_AUTO_DISSENT_TIMER => array('name' => totranslate('Auto Dissent with Timer'), 'cssPref' => 'dissent_timer'),
                 ),
                 'default' => PREFVALUE_AUTO_DISSENT_ON,
                 'server_sync' => true
